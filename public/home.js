3cadbc0581b08879b882a8f1e829a4cea9b9097a
Jacob Walker <jacob.walker94@gmail.com>
2014-01-31 02:57:01 +0000

    Display map with a data point (only one - a bug)

--- public/home.js
+++ public/home.js
@@ -1,11 +1,53 @@
-function initialize() {
+$(function () {
+	// Google Maps
 	var mapOptions = {
 		center: new google.maps.LatLng(-8.485417, 126.87195),
 		zoom: 11,
 		mapTypeId: google.maps.MapTypeId.HYBRID
 	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
+	
+	map = new google.maps.Map(document.getElementById("map-canvas"),
 		mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+	
+	// Load reports as markers
+	markers = [];
+	locations = [];
+	
+	$.ajax({
+		type: "GET",
+		url: "/locations.json",
+		dataType: "json"
+	}).done(function (allLocations) {
+		console.log(allLocations);
+		
+		// thisLocation because location was taken
+		for (var i = 0; i < allLocations.length; i++) {
+			var thisLocation = allLocations[i];
+			console.log(thisLocation);
+			
+			locations[parseInt(thisLocation.id)] = {
+				latitude: thisLocation.latitude,
+				longitude: thisLocation.longitude
+			};
+		}
+		
+		// Daisy chain!
+		$.ajax({
+			type: "GET",
+			url: "/reports.json",
+			dataType: "json"
+		}).done(function (allReports) {
+			for (var i = 0; i < allReports.length; i++) {
+				var report = allReports[i];
+				console.log(report);
+				
+				var reportLocation = locations[parseInt(report.location_id)];
+				markers.push(new google.maps.Marker({
+					position: new google.maps.LatLng(reportLocation.latitude, reportLocation.longitude),
+					map: map,
+					title: report.body
+				}));
+			}
+		});
+	});
+});
