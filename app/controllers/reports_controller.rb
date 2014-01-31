e3e97b8adf56d58c98b6f43eebc9f353e03a3b22
Jacob Walker <jacob.walker94@gmail.com>
2014-01-31 00:13:52 +0000

    Add basic functionality for reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,25 @@
 class ReportsController < ApplicationController
+  def index
+    @reports = Report.all
+  end
+  
+  def new
+    @report = Report.new
+  end
+  
+  def create
+    report = Report.new report_params
+    report.save
+    
+    redirect_to({ action: "show", id: report.id })
+  end
+  
+  def show
+    @report = Report.find_by_id(params[:id])
+  end
+  
+  private
+    def report_params
+      params.require(:report).permit(:body, :location_id)
+    end
 end
